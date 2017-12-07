72dc67cdf0fba5b1f480f8840b744be1e2e52359
ShotaAk <macakasit@gmail.com>
2017-12-07 23:05:42 +0900

    play_force_start, play_kickoff追加

--- decision_making/scripts/plays/play_book.py
+++ decision_making/scripts/plays/play_book.py
@@ -2,6 +2,8 @@
 from play_halt import PlayHalt
 from play_stop import PlayStop
 from play_pre_kickoff import PlayPreKickoff
+from play_kickoff import PlayKickoff
+from play_force_start import PlayForceStart
 
 class PlayBook(object):
 
@@ -9,3 +11,5 @@ class PlayBook(object):
     book.append(PlayHalt())
     book.append(PlayStop())
     book.append(PlayPreKickoff())
+    book.append(PlayKickoff())
+    book.append(PlayForceStart())
