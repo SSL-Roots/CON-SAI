25f6b99d55b092b66515f345ee53a797f27a2658
ShotaAk <macakasit@gmail.com>
2017-12-07 21:04:58 +0900

    tactic_interposeにtargetとbaseを設定できる

--- decision_making/scripts/plays/tactics/tactic_interpose.py
+++ decision_making/scripts/plays/tactics/tactic_interpose.py
@@ -8,9 +8,12 @@ sys.path.append(os.pardir)
 from coordinate import Coordinate
 
 class TacticInterpose(ParallelAll):
-    def __init__(self, name, my_role, to_dist=None, from_dist=None):
+    def __init__(self, name, my_role, base="CONST_OUR_GOAL", target="Ball", 
+            to_dist=None, from_dist=None):
         super(TacticInterpose, self).__init__(name)
 
         self._coordinate = Coordinate()
-        self._coordinate.set_interpose(to_dist = to_dist, from_dist = from_dist)
-        self.add_child(DynamicDrive('DynamicDrive', my_role, self._coordinate))
+        self._coordinate.set_interpose(base = base, target = target, 
+                to_dist = to_dist, from_dist = from_dist)
+        self.add_child(DynamicDrive('DynamicDrive', my_role, self._coordinate,
+            always_running = True))
