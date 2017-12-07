483d3ac6533ca148411b3e0a4da940ef46b8d68a
ShotaAk <macakasit@gmail.com>
2017-12-07 23:15:59 +0900

    play_inplay追加

--- decision_making/scripts/plays/play_book.py
+++ decision_making/scripts/plays/play_book.py
@@ -4,6 +4,7 @@ from play_stop import PlayStop
 from play_pre_kickoff import PlayPreKickoff
 from play_kickoff import PlayKickoff
 from play_force_start import PlayForceStart
+from play_inplay import PlayInPlay
 
 class PlayBook(object):
 
@@ -13,3 +14,4 @@ class PlayBook(object):
     book.append(PlayPreKickoff())
     book.append(PlayKickoff())
     book.append(PlayForceStart())
+    book.append(PlayInPlay())
