b7c77b2bbd9b3083b988f248b38e69b1a40d2341
ShotaAk <macakasit@gmail.com>
2017-12-07 22:31:20 +0900

    play_pre_kickoff追加

--- decision_making/scripts/plays/play_book.py
+++ decision_making/scripts/plays/play_book.py
@@ -1,9 +1,11 @@
 
 from play_halt import PlayHalt
 from play_stop import PlayStop
+from play_pre_kickoff import PlayPreKickoff
 
 class PlayBook(object):
 
     book = [] 
     book.append(PlayHalt())
     book.append(PlayStop())
+    book.append(PlayPreKickoff())
