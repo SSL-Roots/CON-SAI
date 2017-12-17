b4b63a6655d0e804e6c79d9d0843806ad97779dc
ShotaAk <macakasit@gmail.com>
2017-12-17 22:43:12 +0900

    tactic_positionのリファクタリング

--- decision_making/scripts/plays/tactics/skills/drive_to_target.py
+++ decision_making/scripts/plays/tactics/skills/drive_to_target.py
@@ -7,14 +7,14 @@ sys.path.append(os.pardir)
 from world_model import WorldModel
 
 class DriveToTarget(Task):
-    def __init__(self, name, my_role, x, y, yaw):
+    def __init__(self, name, my_role, x, y, theta):
         super(DriveToTarget, self).__init__(name)
         self._my_role = my_role
         self._x = x
         self._y = y
-        self._yaw = yaw
+        self._theta = theta
 
     def run(self):
-        WorldModel.commands[self._my_role].set_target_pose(self._x, self._y, self._yaw, 'map')
+        WorldModel.commands[self._my_role].set_target_pose(self._x, self._y, self._theta, 'map')
 
         return TaskStatus.RUNNING
