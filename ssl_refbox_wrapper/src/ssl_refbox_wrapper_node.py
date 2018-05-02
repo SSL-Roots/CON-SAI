2ec4862d896f5164b32026fcf76af7dbfda02e04
ShotaAk <macakasit@gmail.com>
2018-05-02 14:40:58 +0900

    referee: designed_positionを受信できる

--- ssl_refbox_wrapper/src/ssl_refbox_wrapper_node.py
+++ ssl_refbox_wrapper/src/ssl_refbox_wrapper_node.py
@@ -23,20 +23,30 @@ def convert_team_info(data):
 
     return team_info
 
-def extract_designated_position(protobuf, our_side_is_left):
+def extract_designated_position(protobuf, our_side):
     has_position = False
-    designated_position = Pose()
+    result_pose = Pose()
 
-    if protobuf.has_designated_position():
+    if protobuf.HasField('designated_position'):
         has_position = True
+        designated_position = protobuf.designated_position
 
-    return has_position, designated_position
+        # convert unit: millimeters to meters
+        result_pose.x = 0.001 * designated_position.x
+        result_pose.y = 0.001 * designated_position.y
+
+        if our_side == "RIGHT":
+            result_pose.x *= -1.0
+            result_pose.y *= -1.0
+
+    return has_position, result_pose
 
 
 if  __name__ == '__main__':
     rospy.init_node('ssl_refbox_wrapper_node')
 
     our_color = rospy.get_param('friend_color', 'yellow').upper()
+    our_side = rospy.get_param('team_side', 'right').upper()
     multicast_addr = rospy.get_param('~multicast_addr', '224.5.23.13')
     multicast_port = rospy.get_param('~multicast_port', 10003)
 
@@ -50,6 +60,7 @@ if  __name__ == '__main__':
     pub_command_duration   =  rospy.Publisher('~command_duration', Duration, queue_size = 10)
     pub_blue_info = rospy.Publisher('~blue_info', RefereeTeamInfo, queue_size = 10)
     pub_yellow_info = rospy.Publisher('~yellow_info', RefereeTeamInfo, queue_size = 10)
+    pub_designated_position = rospy.Publisher('~designated_position', Pose, queue_size = 10)
 
     r = rospy.Rate(60)
     while not   rospy.is_shutdown():
@@ -72,4 +83,7 @@ if  __name__ == '__main__':
         yellow_info = convert_team_info(protobuf.yellow)
         pub_yellow_info.publish(yellow_info)
 
+        has_pose, pose = extract_designated_position(protobuf, our_side)
+        if has_pose:
+            pub_designated_position.publish(pose)
 
--- ssl_refbox_wrapper/src/tests/test_ssl_refbox_wrapper.py
+++ ssl_refbox_wrapper/src/tests/test_ssl_refbox_wrapper.py
@@ -7,15 +7,47 @@ import unittest
 pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(pardir)
 
-import ssl_refbox_wrapper_node
+import ssl_refbox_wrapper_node as wrapper
+import referee_pb2
+from consai_msgs.msg import Pose
 
 
 class TestRefboxWrpper(unittest.TestCase):
     def setup(self):
         pass
 
-    def test_init(self):
-        self.assertEqual(True, True)
+    def test_extract_designated_position(self):
+        protobuf = referee_pb2.SSL_Referee()
+        our_side = "LEFT"
+
+        has_pose, pose = wrapper.extract_designated_position(
+                protobuf, our_side)
+
+        expected = False
+        self.assertEqual(expected, has_pose)
+        expected_pose = Pose(0,0,0)
+        self.assertAlmostEqual(expected_pose, pose)
+
+        protobuf.designated_position.x = 1000
+        protobuf.designated_position.y = 2000
+        has_pose, pose = wrapper.extract_designated_position(
+                protobuf, our_side)
+
+        expected = True
+        self.assertEqual(expected, has_pose)
+        expected_pose = Pose(1.0,2.0,0)
+        self.assertAlmostEqual(expected_pose, pose)
+
+        our_side = "RIGHT"
+        has_pose, pose = wrapper.extract_designated_position(
+                protobuf, our_side)
+
+        expected = True
+        self.assertEqual(expected, has_pose)
+        expected_pose = Pose(-1.0,-2.0,0)
+        self.assertAlmostEqual(expected_pose, pose)
+
+
 
 if __name__ == "__main__":
     import rosunit
