d579f42c465130d6df9f09db1cabc4ac6831712e
ShotaAk <macakasit@gmail.com>
2018-05-02 10:26:02 +0900

    constants.posesが動的に更新されない問題を解決

--- decision_making/scripts/constants.py
+++ decision_making/scripts/constants.py
@@ -1,4 +1,5 @@
 from geometry_msgs.msg import Point
+import rospy
 
 ROBOT_NUM = 8
 ID_MAX = 8
@@ -43,9 +44,24 @@ def set_field(length, width):
     FieldHalfX = FieldX * 0.5
     FieldHalfY = FieldY * 0.5
 
+    _set_poses()
 
 def set_penalty(x, y):
     global PenaltyX, PenaltyY
 
     PenaltyX = x
     PenaltyY = y
+
+def _set_poses():
+    global poses
+
+    poses = {
+        'CONST_OUR_GOAL' : Point(-FieldHalfX, 0.0, 0),
+        'CONST_OUR_GOAL_UPPER' : Point(-FieldHalfX, DefenceHalfStreach, 0),
+        'CONST_OUR_GOAL_LOWER' : Point(-FieldHalfX, -DefenceHalfStreach, 0),
+        'CONST_THEIR_GOAL' : Point(FieldHalfX, 0.0, 0),
+        'CONST_THEIR_GOAL_UPPER' : Point(FieldHalfX, DefenceHalfStreach, 0),
+        'CONST_THEIR_GOAL_LOWER' : Point(FieldHalfX, -DefenceHalfStreach, 0),
+        'CONST_THEIR_PENALTY_MARK' : Point(FieldHalfX - DefenceLength, 0.0, 0)
+        }
+
