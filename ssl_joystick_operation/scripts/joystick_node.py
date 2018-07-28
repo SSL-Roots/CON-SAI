999d2ac3fb72783aea0f6488592461be695354bd
ShotaAk <macakasit@gmail.com>
2018-07-28 20:09:18 +0900

    ジョイスティックでロボットを動かせる

--- ssl_joystick_operation/scripts/joystick_node.py
+++ ssl_joystick_operation/scripts/joystick_node.py
@@ -26,35 +26,61 @@ class Core(object):
         self._AXES_X = rospy.get_param('~axes_X')
         self._AXES_Y = rospy.get_param('~axes_Y')
 
+        self._prev_test_name = "TEST_NONE"
+        self._prev_test_command = TestAICommand()
+        self._buttons = []
+
+        self._MAX_VELOCITY = 0.5
+        self._DRIBBLE_POWER = 6
+        self._KICK_POWER = 6
+
     
     def _callback_joy(self, msg):
-        if msg.buttons[self._A]:
-            rospy.logerr("button A")
-        if msg.buttons[self._B]:
-            rospy.logerr("button B")
-        if msg.buttons[self._X]:
-            rospy.logerr("button X")
-        if msg.buttons[self._Y]:
-            rospy.logerr("button Y")
-        if msg.buttons[self._L]:
-            rospy.logerr("button L")
-        if msg.buttons[self._R]:
-            rospy.logerr("button R")
+        test_name = self._prev_test_name
+        test_command = self._prev_test_command
+
         if msg.buttons[self._SEL]:
-            rospy.logerr("button SEL")
+            test_name = "TEST_NONE"
+
         if msg.buttons[self._START]:
-            rospy.logerr("button START")
-        if msg.axes[self._AXES_X]:
-            rospy.logerr("axes x :" + str(msg.axes[self._AXES_X]))
-        if msg.axes[self._AXES_Y]:
-            rospy.logerr("axes y :" + str(msg.axes[self._AXES_Y]))
+            test_name = "TEST2"
+
+        if msg.buttons[self._A]:
+            rospy.logdebug("button A")
+        if msg.buttons[self._X]:
+            rospy.logdebug("button X")
+        if msg.buttons[self._Y]:
+            rospy.logdebug("button Y")
+
+        if test_name == "TEST2":
+            # 全手動操作モード
+            test_command.vel_x = self._MAX_VELOCITY * msg.axes[self._AXES_Y]
+            test_command.vel_y = self._MAX_VELOCITY * msg.axes[self._AXES_X]
+
+            if msg.buttons[self._L]:
+                test_command.vel_yaw = self._MAX_VELOCITY
+            elif msg.buttons[self._R]:
+                test_command.vel_yaw = -1.0 * self._MAX_VELOCITY
+            else:
+                test_command.vel_yaw = 0
+
+            if msg.buttons[self._B]:
+                test_command.dribble_power = self._DRIBBLE_POWER
+            else:
+                test_command.dribble_power = 0
+
+            if msg.buttons[self._Y]:
+                test_command.kick_power = self._KICK_POWER
+            else:
+                test_command.kick_power = 0
 
 
+        self._prev_test_name = test_name
+        self._prev_test_command = test_command
 
-def receiveJoy(joy):
-    buttons.refresh(joy.buttons)
-    # referee.publish(joy)
-    commands.publish(joy)
+        self._pub_name.publish(self._prev_test_name)
+        self._pub_command.publish(self._prev_test_command)
+
 
 def main():
     rospy.init_node('joystick_node')
