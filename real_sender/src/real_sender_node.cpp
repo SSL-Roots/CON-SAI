a562b470647f0cb8b6c8e5da9a9e046b7616774f
ShotaAk <macakasit@gmail.com>
2018-12-08 16:42:39 +0900

    real_sender sends frootsCommand

--- real_sender/src/real_sender_node.cpp
+++ real_sender/src/real_sender_node.cpp
@@ -2,9 +2,11 @@
 #include <ros/console.h>
 #include <iostream>
 #include <string>
+#include <cmath>
 #include <unistd.h>
 
 #include <consai_msgs/robot_commands.h>
+#include <frootspi_msg/FrootsCommand.h>
 
 #include "serial.h"
 #include "serializer.hpp"
@@ -12,14 +14,18 @@
 class Sender
 {
 public:
-    Sender()
-        : mBaudrate_(57600),mPort_("/dev/ttyUSB0"),mSerialOpend(false){
+    Sender(bool noSerial)
+        : mBaudrate_(57600),mPort_("/dev/ttyUSB0"),mSerialOpend(false),
+        mIDLinked(false){
 
-        try{
-            mSerial_ = new serial::Serial(mPort_,mBaudrate_,serial::Timeout::simpleTimeout(1000));
-            mSerialOpend = true;
-        } catch (std::exception &error){
-            ROS_ERROR(" %s", error.what());
+        mNoSerial = noSerial;
+        if(noSerial == false){
+            try{
+                mSerial_ = new serial::Serial(mPort_,mBaudrate_,serial::Timeout::simpleTimeout(1000));
+                mSerialOpend = true;
+            } catch (std::exception &error){
+                ROS_ERROR(" %s", error.what());
+            }
         }
     }
 
@@ -29,27 +35,64 @@ public:
         return mSerialOpend;
     }
 
-    void setID(const int id){
+    void setID(ros::NodeHandle nh, const int id){
         mID_ = id;
+        mIDLinked = true;
+        mPublishTopicName = "robot_" + std::to_string(id) + "/froots_command";
+        mPubFrootsCommand = nh.advertise<frootspi_msg::FrootsCommand>(
+                mPublishTopicName.c_str(), 1);
     }
 
     void callback(const consai_msgs::robot_commandsConstPtr& msg){
 
-        RootsSerializer serializer;
+        if(mNoSerial == false){
+            RootsSerializer serializer;
 
-        float   vel_norm  = hypot(msg->vel_surge, msg->vel_sway),
-            vel_theta = atan2(msg->vel_sway, msg->vel_surge) + M_PI/2,
-            omega     = msg->omega,
-            kick_power= msg->kick_speed_x,
-            dribble_power   = msg->dribble_power;
-        RobotCommand::KickType  kick_type = (msg->kick_speed_z > 0.0) ? RobotCommand::CHIP : RobotCommand::STRAIGHT;
+            float   vel_norm  = hypot(msg->vel_surge, msg->vel_sway),
+                vel_theta = atan2(msg->vel_sway, msg->vel_surge) + M_PI/2,
+                omega     = msg->omega,
+                kick_power= msg->kick_speed_x,
+                dribble_power   = msg->dribble_power;
+            RobotCommand::KickType  kick_type = (msg->kick_speed_z > 0.0) ? RobotCommand::CHIP : RobotCommand::STRAIGHT;
 
-        RobotCommand cmd(mID_, vel_norm, vel_theta, omega, dribble_power, kick_power, kick_type);
+            RobotCommand cmd(mID_, vel_norm, vel_theta, omega, dribble_power, kick_power, kick_type);
 
-        std::string data;
-        serializer.serialize(cmd, &data);
+            std::string data;
+            serializer.serialize(cmd, &data);
 
-        mSerial_->write(data);
+            mSerial_->write(data);
+        }
+
+        // for FrootsPi
+        if(mIDLinked){
+            publishFrootsCommand(msg);
+        }
+    }
+
+    void publishFrootsCommand(const consai_msgs::robot_commandsConstPtr& msg){
+        frootspi_msg::FrootsCommand frootsCommand;
+
+        // 強制的にキック用のコンデンサを充電する
+        frootsCommand.charge_flag = true;
+
+        if(msg->kick_speed_x){
+            frootsCommand.kick_flag = true;
+            frootsCommand.kick_power = msg->kick_speed_x;
+        }
+
+        if(msg->dribble_power){
+            frootsCommand.dribble_power = msg->dribble_power;
+        }
+
+        double norm = std::sqrt(
+                std::pow(msg->vel_surge, 2) + std::pow(msg->vel_sway, 2));
+        double theta = std::atan2(msg->vel_sway, msg->vel_surge);
+
+        frootsCommand.vel_norm = norm;
+        frootsCommand.vel_theta = theta;
+        frootsCommand.vel_omega = msg->omega;
+
+        mPubFrootsCommand.publish(frootsCommand);
     }
 
     
@@ -57,9 +100,13 @@ private:
     const int mBaudrate_;
     const std::string mPort_;
 
+    bool mNoSerial;
     int mID_;
     serial::Serial *mSerial_;
     bool mSerialOpend;
+    bool mIDLinked;
+    ros::Publisher mPubFrootsCommand;
+    std::string mPublishTopicName;
 };
 
 
@@ -69,19 +116,34 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     ros::Rate r(60);
 
-    Sender senders[robot_num];
+    bool noSerial = false;
+    ros::param::get("~no_serial", noSerial);
+
+    if(noSerial){
+        ROS_INFO("NO SERIAL");
+    }else{
+        ROS_INFO("SERIALLLLLL");
+    }
+
+    // Sender senders[robot_num];
+    Sender* senders[robot_num];
     ros::Subscriber subscribers[robot_num];
 
     for(int i=0;i<12;i++){
-        if (!senders[i].portOpend()){
+        senders[i] = new Sender(noSerial);
+
+        // シリアル通信を使う場合は、ポートのチェックをする　
+        if (noSerial == false && !senders[i]->portOpend()){
             ros::shutdown();
         }
 
-        std::stringstream topicStream;
-        topicStream << "robot_" << i << "/robot_commands";
-        std::string topicName = topicStream.str();
-        senders[i].setID(i);
-        subscribers[i] = nh.subscribe(topicName.c_str(),100,&Sender::callback, &senders[i]);
+        senders[i]->setID(nh, i);
+
+        std::string parentName;
+        parentName = "robot_" + std::to_string(i);
+        std::string topicName = parentName + "/robot_commands";
+
+        subscribers[i] = nh.subscribe(topicName.c_str(),100,&Sender::callback, senders[i]);
     }
 
     while (ros::ok()) {
